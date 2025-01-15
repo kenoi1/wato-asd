3008821eac8b39b7e6f49814eb910676e32af2f8
zxTomw <tom_zhixing@outlook.com>
2025-01-14 20:50:27 -0500

    costmap inflate

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,35 +1,36 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
- 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "sensor_msgs/msg/laser_scan.hpp"
+
 #include "costmap_core.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "std_msgs/msg/string.hpp"
 #include <vector>
 
 class CostmapNode : public rclcpp::Node {
-  public:
+public:
     CostmapNode();
-    
+
     // Place callback function here
     void publishMessage();
     void lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
     void initializeCostmap(int size);
     void printOccupancyGrid();
 
-  private:
+private:
     robot::CostmapCore costmap_;
     // Place these constructs here
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
-    
+
     static constexpr double RESOLUTION = 0.1;
     static constexpr double SIZE_OF_MAP = 200;
+    static constexpr double MAX_COST = 100;
     std::pair<int, int> getGridIndicies(std::pair<int, int> origin, double range, double angle);
     std::vector<std::vector<int>> occupancyGrid;
-
+    void inflate(int x, int y, int radius);
 };
- 
-#endif 
+
+#endif
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,66 +1,83 @@
 #include <chrono>
 #include <memory>
+#include <vector>
 
 #include "costmap_node.hpp"
 #include <math.h>
-CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger()))
-{
-  // Initialize the constructs and their parameters
-  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
 
-  lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-      "/lidar", 10,
-      std::bind(&CostmapNode::lidarCallback, this, std::placeholders::_1));
+void CostmapNode::inflate(int radius) {
+    for (int y = 0; y < SIZE_OF_MAP; ++y) {
+        for (int x = 0; x < SIZE_OF_MAP; ++x) {
+            for (int i = -radius; i <= radius; ++i) {
+                for (int j = -radius; j <= radius; ++j) {
+                    if (x + i >= 0 && x + i < SIZE_OF_MAP && y + j >= 0 && y + j < SIZE_OF_MAP) {
+                        const double distance = sqrt(i * i + j * j);
+                        const int cost = MAX_COST * (1 - distance / radius);
+                        if (distance <= radius && occupancyGrid[x + i][y + j] < cost) {
+                            occupancyGrid[x + i][y + j] = cost;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
-  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
+    // Initialize the constructs and their parameters
+    string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
+
+    lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+        "/lidar", 10,
+        std::bind(&CostmapNode::lidarCallback, this, std::placeholders::_1));
+
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
 }
 
 // Define the timer to publish a message every 500ms
-void CostmapNode::publishMessage()
-{
-  auto message = std_msgs::msg::String();
-  message.data = "Hello, ROS 2!";
+void CostmapNode::publishMessage() {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, ROS 2!";
 
-  auto message2 = std_msgs::msg::String();
-  message2.data = "AwawawaawawA";
+    auto message2 = std_msgs::msg::String();
+    message2.data = "AwawawaawawA";
 
-  RCLCPP_INFO(this->get_logger(), "Publishing: '%s' %s", message.data.c_str(), message2.data.c_str());
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s' %s", message.data.c_str(), message2.data.c_str());
 
-  string_pub_->publish(message);
-  string_pub_->publish(message2);
+    string_pub_->publish(message);
+    string_pub_->publish(message2);
 }
 void CostmapNode::lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
-  RCLCPP_INFO(this->get_logger(), "Received LaserScan data:");
+    RCLCPP_INFO(this->get_logger(), "Received LaserScan data:");
 
-  
-  if (occupancyGrid.empty()) {
+    if (occupancyGrid.empty()) {
         initializeCostmap(SIZE_OF_MAP); // 200 cells l * w
     }
 
-  for (size_t i = 0; i < scan->ranges.size(); ++i) {
-    double angle = scan->angle_min + i * scan->angle_increment;
-    double range = scan->ranges[i];
+    for (size_t i = 0; i < scan->ranges.size(); ++i) {
+        double angle = scan->angle_min + i * scan->angle_increment;
+        double range = scan->ranges[i];
 
-    if (range < scan->range_max && range > scan->range_min) { // within grid
-      // int x_grid, y_grid;
-      // convertToGrid(range, range, x_grid, y_grid);
-      // markObstacle(x_grid, y_grid);
+        if (range < scan->range_max && range > scan->range_min) { // within grid
+                                                                  // int x_grid, y_grid;
+                                                                  // convertToGrid(range, range, x_grid, y_grid);
+                                                                  // markObstacle(x_grid, y_grid);
+        }
+
+        // Log the angle
+        // RCLCPP_INFO(this->get_logger(), "Angle: %.2f radians", angle);
     }
-
-    // Log the angle
-    // RCLCPP_INFO(this->get_logger(), "Angle: %.2f radians", angle);
-  }
 }
 
-void CostmapNode::initializeCostmap(int size){ // 0.1m per cell, 200 cells, 20m
-  occupancyGrid.resize(size, std::vector<int>(size, 0)); // set size of array
- 
- // init all cells as 0 (empty)
-  for (int i = 0; i < size; ++i) { 
-        for (int j = 0; j < size; ++j) { 
-            occupancyGrid[i][j] = 0; 
-        } 
-    } 
+void CostmapNode::initializeCostmap(int size) {            // 0.1m per cell, 200 cells, 20m
+    occupancyGrid.resize(size, std::vector<int>(size, 0)); // set size of array
+
+    // init all cells as 0 (empty)
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            occupancyGrid[i][j] = 0;
+        }
+    }
 }
 
 void CostmapNode::printOccupancyGrid() {
@@ -84,10 +101,9 @@ std::pair<int, int> CostmapNode::getGridIndicies(std::pair<int, int> origin, dou
     protectedY = (protectedY < 0) ? 0 : protectedY;
     return {protectedX, protectedY};
 }
-int main(int argc, char **argv)
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<CostmapNode>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<CostmapNode>());
+    rclcpp::shutdown();
+    return 0;
 }
