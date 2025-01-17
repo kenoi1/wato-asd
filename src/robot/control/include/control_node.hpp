0e254f6aeeb5be703f1f539e36fb1816c004d0bb
zxTomw <tom_zhixing@outlook.com>
2025-01-17 03:41:04 +0000

    control node boilerplate

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -2,6 +2,11 @@
 #define CONTROL_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include <nav_msgs/msg/path.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <cmath>
+#include <optional>
 
 #include "control_core.hpp"
 
@@ -10,6 +15,27 @@ class ControlNode : public rclcpp::Node {
     ControlNode();
 
   private:
+    void controlLoop();
+    std::optional<geometry_msgs::msg::PoseStamped> findLookaheadPoint();
+    double computeDistance(const geometry_msgs::msg::Point &a, const geometry_msgs::msg::Point &b);
+    double extractYaw(const geometry_msgs::msg::Quaternion &quat);
+    geometry_msgs::msg::Twist computeVelocity(const geometry_msgs::msg::PoseStamped &target);
+     // Subscribers and Publishers
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+ 
+    // Timer
+    rclcpp::TimerBase::SharedPtr control_timer_;
+ 
+    // Data
+    nav_msgs::msg::Path::SharedPtr current_path_;
+    nav_msgs::msg::Odometry::SharedPtr robot_odom_;
+ 
+    // Parameters
+    double lookahead_distance_;
+    double goal_tolerance_;
+    double linear_speed_;
     robot::ControlCore control_;
 };
 
